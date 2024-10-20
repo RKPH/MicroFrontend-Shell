fed54fa4653fcd7806c4b251ec368fc608eaffe9
RKPH <pnghung2003@gmail.com>
2024-10-20 13:03:11 +0700

    test state in shell

--- src/Store/Counterstore.js
+++ src/Store/Counterstore.js
@@ -1,12 +1,21 @@
 import { create } from "zustand";
 
-const useCounterStore = create((set) => ({
+let store;
+
+const createCounterStore = (set) => ({
   count: 0,
   increaseCount: () =>
     set((state) => {
       console.log("Increasing count:", state.count + 1);
       return { count: state.count + 1 };
     }),
-}));
+});
+
+const useCounterStore = () => {
+  if (!store) {
+    store = create((set) => createCounterStore(set));
+  }
+  return store;
+};
 
 export default useCounterStore;
