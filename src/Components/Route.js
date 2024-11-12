d5fc2401d1c6244021e06ce2461080aa40069dd2
RKPH <pnghung2003@gmail.com>
2024-11-12 23:47:38 +0700

    update react-router-dom

--- src/Components/Route.js
+++ src/Components/Route.js
@@ -1,40 +1,40 @@
-import React, { lazy, Suspense } from "react"; // Import Suspense
-import NoheaderLayout from "../Layout/NoheaderLayout";
+  import React, { lazy, Suspense } from "react"; // Import Suspense
+  import NoheaderLayout from "../Layout/NoheaderLayout";
 
-const HomePage = lazy(() => import("homepage/HomePage"));
-const MenPage = lazy(() => import("ProductPages/MenPage"));
+  const HomePage = lazy(() => import("homepage/HomePage"));
+  const MenPage = lazy(() => import("ProductPages/MenPage"));
 
-const WomenPage = lazy(() => import("ProductPages/WomenPage"));
+  const WomenPage = lazy(() => import("ProductPages/WomenPage"));
 
-const KidPage = lazy(() => import("ProductPages/KidsPage"));
+  const KidPage = lazy(() => import("ProductPages/KidsPage"));
 
-const BrandsPage = lazy(() => import("ProductPages/BrandssPage"));
-const LoginPage = lazy(() => import("homepage/Login"));
+  const BrandsPage = lazy(() => import("ProductPages/BrandssPage"));
+  const LoginPage = lazy(() => import("homepage/Login"));
 
-export const publicRoutes = [
-  {
-    path: "/",
-    component: HomePage,
-  },
-  {
-    path: "/Men",
-    component: MenPage,
-  },
-  {
-    path: "/Women",
-    component: WomenPage,
-  },
-  {
-    path: "/Kid",
-    component: KidPage,
-  },
-  {
-    path: "/Brands",
-    component: BrandsPage,
-  },
-  {
-    path: "/Login",
-    component: LoginPage,
-    layout: NoheaderLayout,
-  },
-];
+  export const publicRoutes = [
+    {
+      path: "/",
+      component: HomePage,
+    },
+    {
+      path: "/Men",
+      component: MenPage,
+    },
+    {
+      path: "/Women",
+      component: WomenPage,
+    },
+    {
+      path: "/Kid",
+      component: KidPage,
+    },
+    {
+      path: "/Brands",
+      component: BrandsPage,
+    },
+    {
+      path: "/Login",
+      component: LoginPage,
+      layout: NoheaderLayout,
+    },
+  ];
