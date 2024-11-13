9e978a97f5b311e9da186284045322cc3478e483
GiaHung11062003 <hung_pham_nguyen_gia@bita.jp>
2024-11-13 09:31:04 +0700

    icon and badge

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -347,7 +347,7 @@ const Header = () => {
                                 />
                             </svg>
                             <span
-                                className="absolute top-0 left-1/2 transform -translate-x-1/2 h-4 w-4 rounded-full bg-yellow-400 text-xs flex items-center justify-center"
+                                className="absolute top-0 left-1/2  h-4 w-4 rounded-full bg-yellow-400 text-xs flex items-center justify-center"
                             >
                               {count}
                             </span>
