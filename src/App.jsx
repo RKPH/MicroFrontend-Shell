d823b74cf61ffaf57a65ebfbea3d4e62d1861af4
GiaHung11062003 <hung_pham_nguyen_gia@bita.jp>
2024-11-13 08:45:28 +0700

    add page transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,50 @@
-import { useState, Fragment } from "react";
+import { Fragment } from "react";
 import DefaultLayout from "./Layout/DefaultLayout";
 import { publicRoutes } from "./Components/Route";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
+import './transitions.css'; // Import CSS for transitions
 
-function App() {
-    const location = useLocation();
+function AnimatedRoutes() {
+    const location = useLocation(); // Now useLocation is correctly inside Router
 
     return (
-        <div>
-            <Router>
-                <TransitionGroup>
-                    <CSSTransition key={location.key} classNames="fade" timeout={300}>
-                        <Routes location={location}>
-                            {publicRoutes.map((route, index) => {
-                                const Page = route.component;
-                                let Layout = DefaultLayout;
+        <TransitionGroup>
+            <CSSTransition key={location.key} classNames="fade" timeout={300}>
+                <Routes location={location}>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        let Layout = DefaultLayout;
 
-                                if (route.layout) {
-                                    Layout = route.layout;
-                                } else if (route.layout === null) {
-                                    Layout = Fragment;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
                                 }
+                            />
+                        );
+                    })}
+                </Routes>
+            </CSSTransition>
+        </TransitionGroup>
+    );
+}
 
-                                return (
-                                    <Route
-                                        key={index}
-                                        path={route.path}
-                                        element={
-                                            <Layout>
-                                                <Page />
-                                            </Layout>
-                                        }
-                                    />
-                                );
-                            })}
-                        </Routes>
-                    </CSSTransition>
-                </TransitionGroup>
-            </Router>
-        </div>
+function App() {
+    return (
+        <Router>
+            <AnimatedRoutes />
+        </Router>
     );
 }
 
