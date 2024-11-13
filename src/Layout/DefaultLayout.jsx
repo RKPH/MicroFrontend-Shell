f7f4d7b7fd427bccb7b61ec35045cd8ccef2e92d
GiaHung11062003 <hung_pham_nguyen_gia@bita.jp>
2024-11-13 11:00:08 +0700

    fix loaidng sreen

--- src/Layout/DefaultLayout.jsx
+++ src/Layout/DefaultLayout.jsx
@@ -1,29 +1,30 @@
 // src/Layouts/DefaultLayout.jsx
 import Header from "../Components/Header";
-import React, { lazy, Suspense } from "react";
+import React, {lazy, Suspense} from "react";
 import "tailwindcss/tailwind.css";
 import PropTypes from "prop-types";
 import Chatbot from "../Components/Chatbot";
 import ErrorBoundary from "../Components/ErrorBoundary";
-import { useLocation } from "react-router-dom";
+import {useLocation} from "react-router-dom";
 
-const DefaultLayout = ({ children }) => {
-  const location = useLocation(); // Get current path
+const DefaultLayout = ({children}) => {
+    const location = useLocation(); // Get current path
 
-  return (
-    <div className="w-full min-h-screen">
-      <Header />
-      {/* Reset ErrorBoundary key when path changes */}
-      <ErrorBoundary key={location.pathname}>
-        <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
-      </ErrorBoundary>
-      <Chatbot />
-    </div>
-  );
+    return (
+        <div className="w-full min-h-screen">
+            <Header/>
+            {/* Reset ErrorBoundary key when path changes */}
+            <ErrorBoundary key={location.pathname}>
+                <Suspense fallback={<div
+                    className="w-full h-full bg-red-200 items-center justify-center">Loading...</div>}>{children}</Suspense>
+            </ErrorBoundary>
+            <Chatbot/>
+        </div>
+    );
 };
 
 DefaultLayout.propTypes = {
-  children: PropTypes.node.isRequired,
+    children: PropTypes.node.isRequired,
 };
 
 export default DefaultLayout;
