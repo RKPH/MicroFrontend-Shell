d9dfb032fe49aae8f527acc22c4e0c698cdcaec6
GiaHung11062003 <hung_pham_nguyen_gia@bita.jp>
2024-11-13 11:26:23 +0700

    add loaind srenn

--- src/Layout/DefaultLayout.jsx
+++ src/Layout/DefaultLayout.jsx
@@ -1,22 +1,45 @@
 // src/Layouts/DefaultLayout.jsx
 import Header from "../Components/Header";
-import React, {lazy, Suspense} from "react";
+import React, {Suspense} from "react";
 import "tailwindcss/tailwind.css";
 import PropTypes from "prop-types";
 import Chatbot from "../Components/Chatbot";
 import ErrorBoundary from "../Components/ErrorBoundary";
 import {useLocation} from "react-router-dom";
+import {ScaleLoader} from "react-spinners";
 
 const DefaultLayout = ({children}) => {
     const location = useLocation(); // Get current path
+
+    // Define default values for loader
+    const color = "#000"; // Set your preferred color
+    const loading = true; // This is the loading state
+    const override = {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+    };
 
     return (
         <div className="w-full min-h-screen">
             <Header/>
             {/* Reset ErrorBoundary key when path changes */}
             <ErrorBoundary key={location.pathname}>
-                <Suspense fallback={<div
-                    className="w-full h-full bg-red-200 items-center justify-center">Loading...</div>}>{children}</Suspense>
+                <Suspense fallback={
+                    <div className="w-full h-full flex items-center justify-center bg-red-200">
+                        <ScaleLoader
+                            color={color}
+                            loading={loading}
+                            cssOverride={override}
+                            size={150}
+                            aria-label="Loading Spinner"
+                            data-testid="loader"
+                        />
+                    </div>
+                }>
+                    {children}
+                </Suspense>
             </ErrorBoundary>
             <Chatbot/>
         </div>
